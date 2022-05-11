319f3774b08d2dbefaef67472535fde78e26a52e
mathugo <hugo.mathh@gmail.com>
2022-05-11 14:50:59 +0000

    UNET pytorch template added

---
+++ director/start_director.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+set -e
+
+fx director start --disable-tls -c director_config.yaml
---
+++ envoy/start_envoy.sh
@@ -0,0 +1,1 @@
+fx envoy start -n env_one --disable-tls --envoy-config-path envoy_config.yaml -dh localhost -dp 50050
