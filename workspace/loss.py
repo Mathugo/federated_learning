4dbd6146ce8f6f5e3512e11a052eeead2d06cde7
mathugo <hugo.mathh@gmail.com>
2022-05-16 13:04:57 +0000

    transfer learning working, different loss functions and backbone

--- workspace/loss.py
+++ workspace/loss.py
@@ -1,15 +1,28 @@
 # Copyright (C) 2021-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-"""Layers for Unet model."""
+"""Layers for Unet model.
+    Loss functions for semantic segmentation"""
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import keras
 import keras.backend as K
 import numpy as np
 
+SMOOTH=1e-6
+
+def iou_numpy(outputs: np.array, labels: np.array):
+    outputs = outputs.squeeze(1)
+    
+    intersection = (outputs & labels).sum((1, 2))
+    union = (outputs | labels).sum((1, 2))
+    
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+    
+    thresholded = np.ceil(np.clip(20 * (iou - 0.5), 0, 10)) / 10
+    
+    return thresholded  # Or thresholded.mean()
 
 #PyTorch Take low learning rate ~ 5e-5 1e-4
 class TverskyLoss(nn.Module):
@@ -33,7 +46,6 @@ class TverskyLoss(nn.Module):
         Tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)  
         
         return 1 - Tversky
-
 #PyTorch
 class IoULoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
@@ -58,6 +70,66 @@ class IoULoss(nn.Module):
                 
         return 1 - IoU
 
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        Dice_BCE = BCE + dice_loss
+        
+        return Dice_BCE     
+#PyTorch
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        
+        return 1 - dice
+
+class FocalTverskyLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(FocalTverskyLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1, alpha=0.5, beta=0.5, gamma=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #True Positives, False Positives & False Negatives
+        TP = (inputs * targets).sum()    
+        FP = ((1-targets) * inputs).sum()
+        FN = (targets * (1-inputs)).sum()
+        
+        Tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)  
+        FocalTversky = (1 - Tversky)**gamma
+                       
+        return FocalTversky
+
 #PyTorch Loss for class imbalanced
 class FocalLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
@@ -66,7 +138,7 @@ class FocalLoss(nn.Module):
     def forward(self, inputs, targets, alpha=0.8, gamma=2, smooth=1):
         
         #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = F.sigmoid(inputs)       
+        inputs = torch.sigmoid(inputs)       
         
         #flatten label and prediction tensors
         inputs = inputs.view(-1)
@@ -89,7 +161,7 @@ def soft_dice_loss(output, target):
     score = 1 - score.sum() / num
     return score
 
-
+# Dice evaluates the overlap rate of prediction results and ground truth; equals to f1 score in definition.
 def soft_dice_coef(output, target):
     """Calculate soft DICE coefficient."""
     num = target.size(0)
@@ -99,7 +171,6 @@ def soft_dice_coef(output, target):
     intersection = m1 * m2
     score = 2.0 * (intersection.sum(1) + 1) / (m1.sum(1) + m2.sum(1) + 1)
     return score.sum()
-
 
 class DoubleConv(nn.Module):
     """Pytorch double conv class."""
@@ -123,7 +194,6 @@ class DoubleConv(nn.Module):
         x = self.conv(x)
         return x
 
-
 class Down(nn.Module):
     """Pytorch nn module subclass."""
 
@@ -139,7 +209,6 @@ class Down(nn.Module):
         """Do forward pass."""
         x = self.mpconv(x)
         return x
-
 
 class Up(nn.Module):
     """Pytorch nn module subclass."""
